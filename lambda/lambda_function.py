c767f85c2dfc2e376e37c2c29b2c67dad6f921d6
ericcoufal <ecoufalleano@gmail.com>
2026-06-23 12:27:57 -0600

    updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from our CICD github actions workflow cursos')
+        'body': json.dumps('Hello updated Lambda from cursor')
     }
